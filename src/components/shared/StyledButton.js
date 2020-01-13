23ee31c19fa1a8c6a21d8782a24e40b8bd1a89ee
Kurt Lopez <kurtdevph@gmail.com>
2020-01-13 18:52:04 +0800

    Button style adjustment

--- src/components/shared/StyledButton.js
+++ src/components/shared/StyledButton.js
@@ -5,6 +5,7 @@ export const StyledButton = withStyles(theme => ({
   root: {
     height: 55,
     color: "#fff",
+    textTransform: "capitalize",
     backgroundColor: "#79cea1",
     "&:hover": {
       backgroundColor: "#79cea1"
